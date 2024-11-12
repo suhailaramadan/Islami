8679cdcd09af1f3169d61adeaeb2bf34b42f1867
suhailaramadan <suhailaramadan23@gmail.com>
2024-11-12 11:27:44 +0200

    save

--- lib/tabs/Radio/radio_tab.dart
+++ lib/tabs/Radio/radio_tab.dart
@@ -9,19 +9,41 @@ class RadioTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(mainAxisAlignment: MainAxisAlignment.center,children: [
-      Image.asset("assets/images/images/radio_image.png"),
-      SizedBox(height: 20,),
-      Text(AppLocalizations.of(context)!.quranRadio,style: Theme.of(context).textTheme.headlineSmall,),
-      SizedBox(height: 25,),
-        Row( mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset("assets/images/images/radio_image.png"),
+        SizedBox(
+          height: 20,
+        ),
+        Text(
+          "إذاعة القرآن الكريم",
+          style: Theme.of(context).textTheme.headlineLarge,
+        ),
+        SizedBox(
+          height: 25,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Icon(Icons.skip_previous,size: 40,color:Theme.of(context).iconTheme.color,),
-            Icon(Icons.play_arrow_rounded,color:Theme.of(context).iconTheme.color,size: 60,),
-            Icon(Icons.skip_next,size: 40,color:Theme.of(context).iconTheme.color,),
+            Icon(
+              Icons.skip_previous,
+              size: 40,
+              color: AppTheme.lightPrimary,
+            ),
+            Icon(
+              Icons.play_arrow_rounded,
+              color: AppTheme.lightPrimary,
+              size: 60,
+            ),
+            Icon(
+              Icons.skip_next,
+              size: 40,
+              color: AppTheme.lightPrimary,
+            ),
           ],
         ),
-    
-    ],);
+      ],
+    );
   }
 }
